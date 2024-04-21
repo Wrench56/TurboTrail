da89474e4cf9778a1da0eca61e0dd6cc10b16515
Wrench56 <dmarkreg@gmail.com>
2024-04-21 02:49:48 -0400

    Rename `client`'s HOME_DIR and logfile

--- client/src-tauri/src/constants.rs
+++ client/src-tauri/src/constants.rs
@@ -1,11 +1,11 @@
 use const_format::concatcp;
 
 #[cfg(target_os = "windows")]
-pub const HOME_DIR: &'static str = "C:/TurboTrail/";
+pub const HOME_DIR: &'static str = "C:/TurboTrace/";
 #[cfg(target_os = "unix")]
-pub const HOME_DIR: &str = "/opt/TurboTrail/";
+pub const HOME_DIR: &str = "/opt/TurboTrace/";
 
 pub const SETTINGS_PATH: &'static str = concatcp!(HOME_DIR, "settings/");
 pub const LOGGING_PATH: &'static str = concatcp!(HOME_DIR, "logs/");
 pub const SETTINGS_JSON: &'static str = concatcp!(SETTINGS_PATH, "settings.json");
-pub const LOGGING_FILE: &'static str = concatcp!(LOGGING_PATH, "log.txt");
+pub const LOGGING_FILE: &'static str = concatcp!(LOGGING_PATH, "client.log");
