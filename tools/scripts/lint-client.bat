7cc01c80d1b9bb235da96dea5ade55873a64a26d
Wrench56 <dmarkreg@gmail.com>
2024-04-27 01:43:49 -0400

    Update `lint-client` scripts

--- tools/scripts/lint-client.bat
+++ tools/scripts/lint-client.bat
@@ -2,6 +2,6 @@ REM Call this from the root of the project folder
 
 cd client\src-tauri
 cargo fmt -- --check
-cargo clippy -- -D warnings
+cargo clippy --all-targets --all-features
 cargo check
 cd ..\..
--- tools/scripts/lint-client.sh
+++ tools/scripts/lint-client.sh
@@ -3,5 +3,5 @@
 # Call this from the root of the project folder
 cd client/src-tauri
 cargo fmt -- --check
-cargo clippy -- -D warnings
+cargo clippy --all-targets --all-features
 cargo check
