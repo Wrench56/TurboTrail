9bfa2e31c24b062979fa394551226a8fb8ca4216
Wrench56 <dmarkreg@gmail.com>
2024-04-26 20:44:41 -0400

    Add basic `client` linter scripts

---
+++ tools/scripts/lint-client.bat
@@ -0,0 +1,6 @@
+REM Call this from the root of the project folder
+
+cd client\src-tauri
+cargo fmt -- --check
+cargo clippy -- -D warnings
+cargo check
---
+++ tools/scripts/lint-client.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Call this from the root of the project folder
+cd client/src-tauri
+cargo fmt -- --check
+cargo clippy -- -D warnings
+cargo check
