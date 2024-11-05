c202afb0931c88e702646f7a190017662eaf7fae
Wrench56 <markdevenyidev@gmail.com>
2024-11-04 19:19:03 -0500

    Fix `parseArguments()` in `LogEntry.java`

--- preprocessor/plugin/src/main/java/io/github/wrench56/turbotrace_preprocessor/preprocess/LogEntry.java
+++ preprocessor/plugin/src/main/java/io/github/wrench56/turbotrace_preprocessor/preprocess/LogEntry.java
@@ -4,6 +4,7 @@ import com.github.javaparser.ast.expr.ObjectCreationExpr;
 import com.github.javaparser.ast.expr.Expression;
 import com.github.javaparser.resolution.model.typesystem.*;
 import com.github.javaparser.resolution.types.*;
+import com.github.javaparser.ast.type.*;
 import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.ast.expr.MethodCallExpr;
 
@@ -51,7 +52,7 @@ public class LogEntry {
       if (argNumber == 0) {
         ++argNumber;
         continue;
-      };
+      }
 
       if (arg.isLambdaExpr()) {
         /* This is a problem */
@@ -61,17 +62,22 @@ public class LogEntry {
       argType = arg.calculateResolvedType();
       if (argType instanceof LazyType) {
         String value = arg.toString();
-        template = Utils.replaceNthOccurrence(template, "{}", argNumber, value.substring(1, value.length() - 1));
-        continue;
+        template = Utils.replaceNthOccurrence(template, "{}", argNumber - 1, value.substring(1, value.length() - 1));
       } else if (argType instanceof ResolvedPrimitiveType) {
         /* Primitives */
         args.put(((ResolvedPrimitiveType) argType).describe());
+      } else if (argType instanceof ReferenceType) {
+        /* References (Strings) */
+        args.put(((ReferenceType) argType).resolve().describe());
       } else if (argType instanceof NullType) {
         /* Ignore and remove from template */
-        template = Utils.replaceNthOccurrence(template, "{}", argNumber, "");
-        continue;
+        template = Utils.replaceNthOccurrence(template, "{}", argNumber - 1, "");
       } else if (argType instanceof ResolvedVoidType) {
         /* TODO: Find out whether we need this */
+      } else {
+        System.out.println("Warning: Unknown wrapper used for argument: " + arg);
+        wrapWithUnknownWrapper(arg);
+        args.put("str");
       }
 
       ++argNumber;
@@ -80,6 +86,7 @@ public class LogEntry {
     /* Remove escaped "-s */
     entry.put("template", template.substring(1, template.length() - 1));
     return args;
+
   }
 
   /* TODO: Use this */
